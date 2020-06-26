281111dfa088f97f46731bffa79893a65697cc84
roguewolf-dev <pranjalnirmala@gmail.com>
2020-06-26 21:42:32 +0530

    devlog - first commit

---
+++ public/home.js
@@ -0,0 +1,31 @@
+const e = React.createElement;
+
+const AppNav = () => (
+   <nav class="navbar navbar-dark bg-dark">
+       <a class="navbar-brand" href="#">Devlog</a>
+        <font color="#cccccc">A Blog for Developer</font>
+       <a role="button" class="btn btn-outline-info navbar-btn" href="/login">Login</a>
+   </nav>
+);
+
+class Home extends React.Component {
+   constructor(props) {
+       super(props);
+   }
+
+   render() {
+       return (
+           <div>
+               <AppNav />
+               <div class="card mt-4" Style="width: 100%;">
+                   <div class="card-body">
+                       Please login to see your posts.
+         </div>
+               </div>
+           </div>
+       );
+   }
+}
+
+const domContainer = document.querySelector('#root');
+ReactDOM.render(e(Home), domContainer);
